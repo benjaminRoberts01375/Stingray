cb05fccc7a4e1c86600296412709a65d684d00b3
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-12 11:52:13 -0500

    Create project

---
+++ Stingray/StingrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StingrayApp.swift
+//  Stingray
+//
+//  Created by Ben Roberts on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StingrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
