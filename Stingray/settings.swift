b8e6d4b32964c45cfeee8eda90856b743f6e0eda
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-12 18:35:04 -0500

    Add additional jellyfin info

--- Stingray/settings.swift
+++ Stingray/settings.swift
@@ -12,23 +12,46 @@ final class SettingsManager {
     private let defaults = UserDefaults.standard
     
     var urlProtocol: HttpProtocol {
-        didSet {
-            defaults.set(urlProtocol.rawValue, forKey: "URL-protocol")
-        }
+        didSet { defaults.set(urlProtocol.rawValue, forKey: DefaultsKeys.urlProtocol.rawValue) }
     }
     
     var urlHostname: String {
-        didSet { defaults.set(urlHostname, forKey: "URL-hostname") }
+        didSet { defaults.set(urlHostname, forKey: DefaultsKeys.urlHostname.rawValue) }
     }
     
     var urlPort: String {
-        didSet { defaults.set(urlPort, forKey: "URL-port") }
+        didSet { defaults.set(urlPort, forKey: DefaultsKeys.urlPort.rawValue) }
+    }
+    
+    var usersName: String {
+        didSet { defaults.set(usersName, forKey: DefaultsKeys.usersName.rawValue)}
+    }
+    
+    var sessionID: String {
+        didSet { defaults.set(sessionID, forKey: DefaultsKeys.sessionID.rawValue)}
+    }
+    
+    var userID: String {
+        didSet { defaults.set(userID, forKey: DefaultsKeys.userID.rawValue)}
+    }
+    
+    var accessToken: String {
+        didSet { defaults.set(accessToken, forKey: DefaultsKeys.accessToken.rawValue) }
+    }
+    
+    var serverID: String {
+        didSet { defaults.set(serverID, forKey: DefaultsKeys.serverID.rawValue) }
     }
     
     init() {
-        self.urlProtocol = HttpProtocol(rawValue: defaults.string(forKey: "URL-protocol") ?? "") ?? .http
-        self.urlHostname = defaults.string(forKey: "URL-hostname") ?? ""
-        self.urlPort = defaults.string(forKey: "URL-port") ?? "8096"
+        self.urlProtocol = HttpProtocol(rawValue: defaults.string(forKey: DefaultsKeys.urlProtocol.rawValue) ?? "") ?? .http
+        self.urlHostname = defaults.string(forKey: DefaultsKeys.urlHostname.rawValue) ?? ""
+        self.urlPort = defaults.string(forKey: DefaultsKeys.urlPort.rawValue) ?? "8096"
+        self.usersName = defaults.string(forKey: DefaultsKeys.usersName.rawValue) ?? ""
+        self.sessionID = defaults.string(forKey: DefaultsKeys.sessionID.rawValue) ?? ""
+        self.userID = defaults.string(forKey: DefaultsKeys.userID.rawValue) ?? ""
+        self.accessToken = defaults.string(forKey: DefaultsKeys.accessToken.rawValue) ?? ""
+        self.serverID = defaults.string(forKey: DefaultsKeys.serverID.rawValue) ?? ""
     }
     
     var url: URL? {
@@ -57,4 +80,15 @@ final class SettingsManager {
 enum HttpProtocol: String, CaseIterable {
     case http = "http"
     case https = "https"
+}
+
+enum DefaultsKeys: String {
+    case urlProtocol = "URL-Protocol"
+    case urlHostname = "URL-Hostname"
+    case urlPort = "URL-Port"
+    case usersName = "Users-Name"
+    case sessionID = "Session-ID"
+    case userID = "User-ID"
+    case accessToken = "Access-Token"
+    case serverID = "Server-ID"
 }
