d4fa448e8acba84abc9d79cc2f31bc921d61aebe
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-14 09:05:32 -0500

    Display waiting, error, and library states

--- Stingray/DashboardView.swift
+++ Stingray/DashboardView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct DashboardView: View {
+    enum LibraryStatus {
+        case waiting
+        case available([LibraryModel])
+        case error(Error)
+    }
+    
     @State var streamingService: StreamingServiceProtocol
+    @State var libraries: LibraryStatus = .waiting
+    
     
     var body: some View {
-        Text(streamingService.url?.absoluteString ?? "Missing URL")
+        VStack {
+            switch libraries {
+            case .waiting:
+                ProgressView()
+            case .error(let err):
+                Text("Error loading libraries: \(err.localizedDescription)")
+                    .foregroundStyle(.red)
+                
+            case .available(let libraries):
+                ForEach(libraries.indices, id: \.self) { index in
+                    Text(libraries[index].title)
+                }
+            }
+        }
+        .onAppear {
+            Task {
+                do {
+                    libraries = .available(try await streamingService.getLibraries())
+                } catch {
+                    libraries = .error(error)
+                }
+            }
+        }
     }
 }
