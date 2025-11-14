df369b33cf68b9d91a348ef027e55ace40d643ab
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-14 15:41:55 -0500

    Move image tags into separate struct

--- Stingray/MediaModel.swift
+++ Stingray/MediaModel.swift
@@ -10,41 +10,40 @@ import Foundation
 public protocol MediaProtocol {
     var title: String { get }
     var description: String { get }
-    var logoArtURL: String { get }
-    var boxArtURL: String { get }
-    var backgroundArtURL: String { get }
+    var ImageTags: MediaImages { get }
 }
 
 @Observable
 final class MediaModel: MediaProtocol, Decodable {
     var title: String
+    var tagline: String
     var description: String
-    var logoArtURL: String
-    var boxArtURL: String
-    var backgroundArtURL: String
+    var ImageTags: MediaImages
     
     enum CodingKeys: String, CodingKey {
         case title
+        case tagline
         case description
-        case logoArtURL
-        case boxArtURL
-        case backgroundArtURL
-    }
-    
-    init(title: String, description: String, logoArtURL: String, boxArtURL: String, backgroundArtURL: String) {
-        self.title = title
-        self.description = description
-        self.logoArtURL = logoArtURL
-        self.boxArtURL = boxArtURL
-        self.backgroundArtURL = backgroundArtURL
+        case imageThumbnail
+        case imageLogo
+        case imagePrimary
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.title = try container.decode(String.self, forKey: .title)
+        self.tagline = try container.decode(String.self, forKey: .tagline)
         self.description = try container.decode(String.self, forKey: .description)
-        self.logoArtURL = try container.decode(String.self, forKey: .logoArtURL)
-        self.boxArtURL = try container.decode(String.self, forKey: .boxArtURL)
-        self.backgroundArtURL = try container.decode(String.self, forKey: .backgroundArtURL)
+        self.ImageTags = MediaImages(
+            thumbnail: try container.decode(String.self, forKey: .imageThumbnail),
+            logo: try container.decode(String.self, forKey: .imageLogo),
+            primary: try container.decode(String.self, forKey: .imagePrimary)
+        )
     }
+}
+
+public struct MediaImages {
+    var thumbnail: String?
+    var logo: String?
+    var primary: String?
 }
