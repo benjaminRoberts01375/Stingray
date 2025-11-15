5b0dea3f1a57d004be927f7acbcb012e56dc735f
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-14 23:59:52 -0500

    Add no available image placeholder

--- Stingray/Library/MediaCard.swift
+++ Stingray/Library/MediaCard.swift
@@ -14,22 +14,27 @@ struct MediaCard: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            AsyncImage(url: streamingService.networkAPI.getMediaImageURL(accessToken: streamingService.accessToken ?? "", imageType: .primary, imageID: media.id)) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-            } placeholder: {
-                if let blurHash = media.imageBlurHashes?.getBlurHash(for: .primary),
-                   let blurImage = UIImage(blurHash: blurHash, size: .init(width: 32, height: 32)) {
-                    Image(uiImage: blurImage)
+            if media.ImageTags.primary != nil {
+                AsyncImage(url: streamingService.networkAPI.getMediaImageURL(accessToken: streamingService.accessToken ?? "", imageType: .primary, imageID: media.id)) { image in
+                    image
                         .resizable()
                         .scaledToFill()
-                } else {
-                    MediaCardLoading()
+                } placeholder: {
+                    if let blurHash = media.imageBlurHashes?.getBlurHash(for: .primary),
+                       let blurImage = UIImage(blurHash: blurHash, size: .init(width: 32, height: 32)) {
+                        Image(uiImage: blurImage)
+                            .resizable()
+                            .scaledToFill()
+                    } else {
+                        MediaCardLoading()
+                    }
                 }
+                .aspectRatio(3/2, contentMode: .fit)
+                .clipped()
+            } else {
+                MediaCardNoImage()
+                    .aspectRatio(3/2, contentMode: .fit)
             }
-            .aspectRatio(3/2, contentMode: .fit)
-            .clipped()
             
             Text(media.title)
                 .multilineTextAlignment(.center)
@@ -47,6 +52,22 @@ struct MediaCardLoading: View {
             VStack {
                 ProgressView()
                 Text("Getting thumbnail")
+            }
+        }
+    }
+}
+
+struct MediaCardNoImage: View {
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.15)
+            VStack(spacing: 8) {
+                Image(systemName: "photo")
+                    .font(.system(size: 48))
+                    .foregroundStyle(.secondary)
+                Text("No image available")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
             }
         }
     }
