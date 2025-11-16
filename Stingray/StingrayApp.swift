688b0eba64f01c9fb28e7f32e37bcf4e29cc7ddd
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-16 18:08:24 -0500

    Remove needless variables

--- Stingray/StingrayApp.swift
+++ Stingray/StingrayApp.swift
@@ -10,15 +10,10 @@ import SwiftUI
 @main
 struct StingrayApp: App {
     init() {
-        let memoryCapacity = 100 * 1024 * 1024 // 100 MB
-        let diskCapacity = 1024 * 1024 * 1024 // 1 GB
-        
-        let cache = URLCache(
-            memoryCapacity: memoryCapacity,
-            diskCapacity: diskCapacity
+        URLCache.shared = URLCache(
+            memoryCapacity: 100 * 1024 * 1024, // 100 MB
+            diskCapacity: 1024 * 1024 * 1024 // 1 GB
         )
-        
-        URLCache.shared = cache
     }
     
     var body: some Scene {
