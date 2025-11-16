428b61a40d4d6c46b637282481f449433c135297
benjaminroberts01375 <benroberts01375@gmail.com>
2025-11-16 16:27:35 -0500

    Implement caching

--- Stingray/StingrayApp.swift
+++ Stingray/StingrayApp.swift
@@ -9,6 +9,18 @@ import SwiftUI
 
 @main
 struct StingrayApp: App {
+    init() {
+        let memoryCapacity = 100 * 1024 * 1024 // 100 MB
+        let diskCapacity = 1024 * 1024 * 1024 // 1 GB
+        
+        let cache = URLCache(
+            memoryCapacity: memoryCapacity,
+            diskCapacity: diskCapacity
+        )
+        
+        URLCache.shared = cache
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
