109fd453fb9a7c622f76b703d8f6035f944db2cf
benjaminroberts01375 <benroberts01375@gmail.com>
2025-12-11 15:10:05 -0500

    Stub advanced network types

---
+++ TopShelf/APINetwork.swift
@@ -0,0 +1,34 @@
+//
+//  APINetwork.swift
+//  TopShelf
+//
+//  Created by Ben Roberts on 12/11/25.
+//
+
+import Foundation
+
+public protocol AdvancedNetwork {
+    /// Retrieve recently added media of some type
+    /// - Parameters:
+    ///   - contentType: Type of media to retrieve
+    ///   - accessToken: Access token for the server
+    /// - Returns: A silm verion of the media type
+    func getRecentlyAdded(accessToken: String) async throws -> [SlimMedia]
+    
+    /// Gets up next shows
+    /// - Parameter accessToken: Access token for the server
+    /// - Returns: Available media for up next
+    func getUpNext(accessToken: String) async throws -> [SlimMedia]
+}
+
+public struct APINetwork: AdvancedNetwork {
+    var network: BasicNetworkProtocol
+    
+    public func getRecentlyAdded(accessToken: String) async throws -> [SlimMedia] {
+        return []
+    }
+    
+    public func getUpNext(accessToken: String) async throws -> [SlimMedia] {
+        return []
+    }
+}
