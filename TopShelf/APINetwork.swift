1647a6731e30a72c9b9af472eae911878c0963f8
benjaminroberts01375 <benroberts01375@gmail.com>
2025-12-11 15:54:30 -0500

    Added getMediaImageURL

--- TopShelf/APINetwork.swift
+++ TopShelf/APINetwork.swift
@@ -19,16 +19,58 @@ public protocol AdvancedNetworkProtocol {
     /// - Parameter accessToken: Access token for the server
     /// - Returns: Available media for up next
     func getUpNext(accessToken: String) async throws -> [SlimMedia]
+    
+    /// Generates a URL for an image
+    /// - Parameters:
+    ///   - accessToken: Access token for the server
+    ///   - imageType: Type of image (ex. poster)
+    ///   - mediaID: ID of the image
+    ///   - width: Ideal width of the image
+    /// - Returns: Formatted URL if possible
+    func getMediaImageURL(accessToken: String, imageType: MediaImageType, mediaID: String, width: Int) -> URL?
 }
 
 public struct APINetwork: AdvancedNetworkProtocol {
     var network: BasicNetworkProtocol
     
     public func getRecentlyAdded(accessToken: String) async throws -> [SlimMedia] {
-        return []
+        let params: [URLQueryItem] = [
+            URLQueryItem(name: "limit", value: "\(25)"),
+            URLQueryItem(name: "fields", value: "ParentId")
+        ]
+        
+        return try await network.request(
+            verb: .get,
+            path: "/Items/Latest",
+            headers: ["X-MediaBrowser-Token": accessToken],
+            urlParams: params,
+            body: nil
+        )
     }
     
     public func getUpNext(accessToken: String) async throws -> [SlimMedia] {
-        return []
+        struct Root: Decodable {
+            let Items: [SlimMedia]
+        }
+        
+        let params: [URLQueryItem] = [ URLQueryItem(name: "fields", value: "ParentId") ]
+        
+        let root: Root = try await network.request(
+            verb: .get,
+            path: "/Shows/NextUp",
+            headers: ["X-MediaBrowser-Token": accessToken],
+            urlParams: params,
+            body: nil
+        )
+        return root.Items
+    }
+    
+    public func getMediaImageURL(accessToken: String, imageType: MediaImageType, mediaID: String, width: Int) -> URL? {
+        let params : [URLQueryItem] = [
+            URLQueryItem(name: "fillWidth", value: String(width)),
+            URLQueryItem(name: "quality", value: "95")
+        ]
+        
+        return network.buildURL(path: "/Items/\(mediaID)/Images/\(imageType.rawValue)", urlParams: params)
     }
 }
--- TopShelf/StreamingService.swift
+++ TopShelf/StreamingService.swift
@@ -31,11 +31,24 @@ public final class StreamingServiceBasicModel: StreamingServiceBasicProtocol {
     }
     
     public func retrieveRecentlyAdded(_ contentType: RecentlyAddedMediaType) async -> [SlimMedia] {
-        return []
+        do {
+            return try await networkAPI.getRecentlyAdded(accessToken: accessToken)
+        } catch {
+            return []
+        }
     }
     
     public func retrieveUpNext() async -> [SlimMedia] {
-        return []
+        do {
+            guard let upNext = try await networkAPI.getUpNext(accessToken: accessToken).first else { return [] }
+            return [upNext]
+        } catch {
+            return []
+        }
+    }
+    
+    public func getImageURL(imageType: MediaImageType, mediaID: String, width: Int) -> URL? {
+        return networkAPI.getMediaImageURL(accessToken: accessToken, imageType: imageType, mediaID: mediaID, width: width)
     }
 }
 
