39da61c17816352f2aff81743e9058fde7c83bfb
benjaminroberts01375 <benroberts01375@gmail.com>
2025-12-11 15:03:22 -0500

    Create template top shelf files

---
+++ TopShelf/ContentProvider.swift
@@ -0,0 +1,17 @@
+//
+//  ContentProvider.swift
+//  TopShelf
+//
+//  Created by Ben Roberts on 12/11/25.
+//
+
+import TVServices
+
+class ContentProvider: TVTopShelfContentProvider {
+
+    override func loadTopShelfContent() async -> (any TVTopShelfContent)? {
+        // Fetch and return content asynchronously.
+        return nil
+    }
+
+}
