08e71f26708404fb3121d0dc1305f36e9eaefd33
benjaminroberts01375 <benroberts01375@gmail.com>
2025-12-12 20:29:02 -0500

    Remove redundant VStack

--- Stingray/Library/SearchView.swift
+++ Stingray/Library/SearchView.swift
@@ -13,12 +13,10 @@ public struct SearchView: View {
     @State var searchResults: SearchStatus = .empty
     
     public var body: some View {
-        VStack {
+        ScrollView {
             switch searchResults {
             case .found(let allMedia):
-                ScrollView {
-                    MediaGridView(allMedia: allMedia, streamingService: streamingService)
-                }
+                MediaGridView(allMedia: allMedia, streamingService: streamingService)
             case .temporarilyNotFound:
                 ProgressView("Not found yet, but we're still getting your media...")
             case .notFound:
