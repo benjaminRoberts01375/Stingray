2e41c01085f8cc5b21fb95dabe372ac745389f00
benjaminroberts01375 <benroberts01375@gmail.com>
2026-01-28 01:22:43 -0500

    Allow fields to not add to the error bucket

--- Stingray/Models/DecoderExtension.swift
+++ Stingray/Models/DecoderExtension.swift
@@ -16,15 +16,22 @@ extension KeyedDecodingContainer {
     ///   - defaultValue: Fallback value to use if unable to decode
     ///   - errBucket: Bucket to append errors to
     ///   - errLabel: Label denoting the object which owns this key
+    ///   - required: The field is a required field, and will add an error if missing. Default is true
     /// - Returns: The decoded value for the given key, if possible
     func decodeFieldSafely<T: Decodable>(
         _ type: T.Type,
         forKey key: Key,
         default defaultValue: T,
         errBucket: inout [RError],
-        errLabel: String
+        errLabel: String,
+        required: Bool = true,
     ) -> T {
-        do { return try decodeIfPresent(type, forKey: key) ?? { throw JSONError.missingKey(key.description, errLabel) }() }
+        do {
+            return try decodeIfPresent(type, forKey: key) ?? {
+                if required { throw JSONError.missingKey(key.description, errLabel) }
+                return defaultValue
+            }()
+        }
         catch let error as JSONError { // Missing key
             errBucket.append(error)
             return defaultValue
