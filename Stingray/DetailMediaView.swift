458253374b5153c041b8a6ad2eba35e6df889a3d
benjaminroberts01375 <benroberts01375@gmail.com>
2026-02-05 15:34:16 -0500

    Move getNextUp into array extension
    While only used in the DetailMediaView, it's not something relevant to rendering

--- Stingray/DetailMediaView.swift
+++ Stingray/DetailMediaView.swift
@@ -89,7 +89,7 @@ public struct DetailMediaView: View {
                                 }
                             }
                             .task {
-                                if let nextEpisodeID = Self.getNextUp(from: seasons)?.id {
+                                if let nextEpisodeID = seasons.nextUp()?.id {
                                     svrProxy.scrollTo(nextEpisodeID, anchor: .center)
                                 }
                             }
@@ -227,39 +227,6 @@ public struct DetailMediaView: View {
             PlayerView(vm: vm, navigation: $navigation)
         }
     }
-    
-    static func getNextUp(from seasons: [any TVSeasonProtocol]) -> (any TVEpisodeProtocol)? {
-        let allEpisodes = seasons.flatMap(\.episodes)
-        // If the first episode hasn't been played suggest it
-        if let firstEpisode = allEpisodes.first, firstEpisode.lastPlayed == nil {
-            return firstEpisode
-        }
-        // Get the most recent episode
-        guard let mostRecentEpisode = (allEpisodes.enumerated().max { previousEpisode, currentEpisode in
-            return previousEpisode.element.lastPlayed ?? .distantPast < currentEpisode.element.lastPlayed ?? .distantPast
-        }),
-              let mostRecentMediaSource = mostRecentEpisode.element.mediaSources.first
-        else { return allEpisodes.first } // failing getting the most recent, return the first episode
-        
-        // Watched previous episode all the way through
-        if mostRecentMediaSource.startPoint == 0 {
-            if mostRecentEpisode.offset + 1 > allEpisodes.count - 1 {
-                return allEpisodes.first ?? mostRecentEpisode.element
-            }
-            return allEpisodes[mostRecentEpisode.offset + 1]
-        }
-        
-        // Likely marked by Stingray that the user didn't finish
-        if mostRecentMediaSource.startPoint < 0.9 * mostRecentMediaSource.duration {
-            return mostRecentEpisode.element
-        }
-        
-        // User finished the series, recommend the first episode again
-        if mostRecentEpisode.offset + 1 > allEpisodes.count - 1 {
-            return allEpisodes.first ?? mostRecentEpisode.element
-        }
-        return allEpisodes[mostRecentEpisode.offset + 1]
-    }
 }
 
 // MARK: Background
@@ -381,7 +348,7 @@ fileprivate struct PlayNavigationView: View {
             
         case .tv(let seasons):
             guard let seasons = seasons,
-                  let nextEpisode = DetailMediaView.getNextUp(from: seasons)
+                  let nextEpisode = seasons.nextUp()
             else {
                 self.title = "Error"
                 self.mediaSources = []
